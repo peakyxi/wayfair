6a376c76252f35103cba431ff49e8674bebcf7d4
Bruceci <bruceci18217016834@gmail.com>
2021-08-12 04:14:04 +0800

    productscraper

--- models/product.js
+++ models/product.js
@@ -2,6 +2,8 @@ import mongoose from 'mongoose'
 
 const schema = new mongoose.Schema({
 
+    cateIds: { type: [String], default: [], required: true },
+    cateNames: { type: [String], default: [], required: true }
 }, { strict: false })
 
 const Product = mongoose.model('product', schema)
