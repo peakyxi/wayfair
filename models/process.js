6f6e84af58a430c12d6dc83ed890910b3d64875b
Bruceci <bruceci18217016834@gmail.com>
2021-08-15 04:40:45 +0800

    product scraper

--- models/process.js
+++ models/process.js
@@ -6,7 +6,7 @@ const schema = new mongoose.Schema({
     "statusCode": { type: Number, required: true, default: 1 },
     "status": { type: String, required: false, default: 'reset' },
     "error": { type: String, required: false, default: null },
-    "position": { type: Object, required: false, default: { pageIndex: 0, listIndex: 0, itemIndex: 0 } },
+    "position": { type: Object, required: true, default: { urlIndex: 0, pageIndex: 0, itemIndex: 0 } },
     "updated": { type: Date, required: true, default: Date.now() }
 })
 
